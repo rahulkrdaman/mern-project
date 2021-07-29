f385905668a4e37d97083457995be962443b0b58
Rahul Kumar <rahulk@eteaminc.com>
2021-07-28 20:14:53 -0700

    Server.js file update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require("./config/db");
 const app = express();
+const path = require('path');
 
 
 //Connect Database
@@ -14,7 +15,7 @@ app.use(cors());
 app.use(express.urlencoded()); // Parse URL-encoded bodies
 app.use(express.json());// User to parese JSON bodies
 
-app.get("/", (req, res) => res.send("Eteam App is running") );
+//app.get("/", (req, res) => res.send("Eteam App is running") );
 
 // Define Routes
 
@@ -23,6 +24,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+//Serve static assets in production
+if(process.env.NODE_ENV === 'Production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
