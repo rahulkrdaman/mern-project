1fab7659b4e3fd2a0cf4eff9b6a2804ab2d40241
Rahul Kumar <rahulk@eteaminc.com>
2021-07-28 18:53:52 -0700

    First Commit For Production

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+import React, {Fragment, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+//Redux Provider
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import AddEducation from './components/profile-forms/AddEducation';
+import Posts from './components/posts/Posts';
+
+
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+function App() {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} /> 
+          <section className='container'>
+            <Alert></Alert>
+            <Switch>
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/profiles" component={Profiles}/>
+              <Route exact path="/profile/:id" component={Profile}/>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
+              <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
+              <PrivateRoute exact path="/add-experience" component={AddExperience}/>
+              <PrivateRoute exact path="/add-education" component={AddEducation}/>
+              <PrivateRoute exact path="/posts" component={Posts}/>
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
